7d1d554b8d1804247cd83175f00f988cf30a9b15
BAUIRJANDeV <mamatalivbauirjan@email.com>
2025-08-22 08:32:55 +0500

    test uchun ozgartirdim

--- users/models.py
+++ users/models.py
@@ -8,8 +8,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     profile = models.ImageField(upload_to="profile_images/", default="defaults/profile.png")
     cover = models.ImageField(upload_to="cover_images/",default="defaults/cover.png")
-    followers = models.ManyToManyField("self", blank=True)
-    following  = models.ManyToManyField("self", blank=True)
+    # followers = models.ManyToManyField("self", blank=True)
+    # following  = models.ManyToManyField("self", blank=True)
     verified = models.BooleanField(default=False)
     bio = models.CharField(max_length=120, blank=True, null=True)
     birth_of_date = models.DateField(null=True, blank=True)
@@ -17,5 +17,15 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+class Follwing(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='followers')
+    follower=models.ForeignKey(User,on_delete=models.CASCADE,related_name='followings')
+    datatime=models.DateTimeField(auto_now_add=True)
 
+
+    def __str__(self):
+        return f"{self.follower.username}  followed {self.user.username}"
+
+    class Meta:
+        unique_together=['user','follower']
 
