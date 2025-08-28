0dc57750b0757487c0522d3d8045639ca0ea170e
Abduolimov03 <asadbekabduolimov33@gmail.com>
2025-08-28 13:48:01 +0500

    create post

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
-
+from .views import IndexView, create_post
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("post/new/", create_post, name="create_post"),
+    # path("posts/", post_list, name="post_list"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
-from django.views import View
-from django.contrib.auth.mixins import LoginRequiredMixin
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from django.views.generic import TemplateView
+from .models import Post
 
 
-class IndexView(LoginRequiredMixin,View):
-    def get(self, request):
-        return render(request, "index.html")
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        body = request.POST.get("body")
+        media = request.FILES.get("media")
+        if body:
+            post = Post.objects.create(user=request.user, body=body, media=media)
+            return redirect("index")
+    return redirect("index")
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.all().order_by("-created")
+        return context
